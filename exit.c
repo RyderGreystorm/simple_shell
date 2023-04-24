536fc21ceda58603fa9692c99c0f50c4d0b6077a
Godbless Biekro <biekrogodbless@gmail.com>
2023-04-24 22:35:39 +0000

    debugging strtok

--- exit.c
+++ exit.c
@@ -8,6 +8,16 @@
 
 void exit_func(char **args)
 {
-	(void) (args);
-	exit(0);
+	char *ptr;
+	int status;
+
+	if (args[0] == NULL)
+		exit(EXIT_SUCCESS);
+	status = strtol(args[0], &ptr, 10);
+	if (*ptr != '\0' || errno == ERANGE)
+	{
+		fprintf(stderr, "Invalid exit status: %s\n", args[0]);
+		exit(EXIT_FAILURE);
+	}
+	exit(status);
 }
