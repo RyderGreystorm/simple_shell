30a1c4d8a9fcc7083ae195204a36ac8a2af4fc6b
Biekro Godbless <biekrogodbless@gmail.com>
2023-04-28 06:54:38 -0700

    reset to previous statble version

--- main.c
+++ main.c
@@ -1,5 +1,81 @@
 #include "main.h"
 
+	char *err_name ;
+	int hist;
+	aliases_t *aliases;
+
+/**
+ * sig_handler - waits for signal
+ * @sign: signal.
+ */
+void sig_handler(int sign)
+{
+	char *err_name __attribute__((unused));
+	int hist __attribute__((unused));
+	aliases_t *aliases __attribute__((unused));
+	char *new_prompt = "\nFavShell$ ";
+
+	(void)sign;
+	signal(SIGINT, sig_handler);
+	write(STDIN_FILENO, new_prompt, 6);
+}
+
+/**
+ * execute - executes a command
+ * @args: args array
+ * @bufptr: pointer to buffer
+ * Return: status of query
+ */
+int execute(char **args, char **bufptr)
+{
+	pid_t pid;
+	int status, flag = 0, rtn = 0;
+	char *cmd = args[0];
+
+	if (cmd[0] != '/' && cmd[0] != '.')
+	{
+		flag = 1;
+		cmd = get_location(cmd);
+	}
+
+	if (!cmd || (access(cmd, F_OK) == -1))
+	{
+		if (errno == EACCES)
+			rtn = (handle_error(args, 126));
+		else
+			rtn = (handle_error(args, 127));
+	}
+	else
+	{
+		pid = fork();
+		if (pid == -1)
+		{
+			if (flag)
+				free(cmd);
+			perror("Error child:");
+			return (1);
+		}
+		if (pid == 0)
+		{
+			execve(cmd, args, environ);
+			if (errno == EACCES)
+				rtn = (handle_error(args, 126));
+			free_env();
+			free_args(args, bufptr);
+			free_alias_list(aliases);
+			_exit(rtn);
+		}
+		else
+		{
+			wait(&status);
+			rtn = WEXITSTATUS(status);
+		}
+	}
+	if (flag)
+		free(cmd);
+	return (rtn);
+}
+
 /**
  * main - funtion to ru the shell
  * @argc: The number of arguments supplied to the program.
